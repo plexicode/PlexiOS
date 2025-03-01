e4488cfcb5319fb0f45c14e05c27fa242531e684
Blake O'Hare <mspaintmaestro@gmail.com>
2025-03-01 21:38:42 +0900

    Convert plexic errors into stderr messages instead of throwing exceptions

--- tools/plexic/index.js
+++ tools/plexic/index.js
@@ -1,14 +1,18 @@
 const APP_MAIN = async (os, procInfo, args) => {
-  
+
   const PlexiScript = (await HtmlUtil.loadComponent('PlexiScript_compile_0_1_0'))();
+
+  let failWithError = async (msg, useStdErr) => {
+    let output = useStdErr ? procInfo.stderr : procInfo.stdout;
+    await output.writeln(msg);
+  };
 
   let projFile = args[0];
   if (!projFile || typeof projFile !== 'string') {
-    await procInfo.stdout.writeln(PlexiScript.getUsageNotes().trimEnd());
-    return;
+    return failWithError(PlexiScript.getUsageNotes().trimEnd(), false);
   }
   if (!projFile.toLowerCase().endsWith('.json')) {
-    throw new Error("TODO: Error for when the project file is not a .json file.");
+    return failWithError('Project file must be a .json file');
   }
 
   let projFileName = projFile.split('/').pop();
@@ -19,47 +23,55 @@ const APP_MAIN = async (os, procInfo, args) => {
 
   let { error, ok, data } = await fs.fileReadJson(projFile);
   if (!ok) {
-    if (error === 'NON_JSON_FILE') throw new Error("TODO: error for when the proj config is not a JSON file");
-    if (error === 'PATH_NOT_FOUND') throw new Error("TODO: error for when proj file does not exist");
-    // TODO: go through rest of possible errors
-    throw new Error();
+    if (error === 'NON_JSON_FILE') return failWithError("Project file did not contain valid JSON.");
+    if (error === 'PATH_NOT_FOUND') return failWithError("Project file could not be found.");
+    return failWithError("Error occurred while reading project file."); // should not happen.
   }
 
   if (!data || typeof data !== 'object' || Array.isArray(data)) {
-    throw new Error('TODO: Error for when the root of the project config is not a real object.');
+    return failWithError("Project file contains invalid content.");
   }
 
   let srcByModule = {};
   let mainModule = data.mainModule;
   if (!mainModule || typeof mainModule !== 'string') {
-    throw new Error('TODO: error for when the main module is not present or not a string');
+    return failWithError("Project file does not designate a mainModule value.");
   }
-  
+
   let dstPath = data.destinationFile ?? ('bin/' + projId);
   if (!dstPath || typeof dstPath !== 'string') {
-    throw new Error('TODO: error for when the dstPath is not a string');
+    return failWithError("Project file contains an invalid value for dstPath");
   }
   dstPath = fs.getParent(fs.join(dstPath, 'dummy')); // canonicalize
-  
+
   for (let moduleInfo of data.modules) {
     let { name, source } = moduleInfo;
-    if (!name || typeof name !== 'string') throw new Error("TODO: error for when a module entry does not have a name present");
-    // TODO: check validity of module name 
-    if (!source || typeof source !== 'string') throw new Error("TODO: error for when the module info does not have a directory present.");
-    let moduleDir = fs.getParent(fs.join(projectDir, source, 'dummypath')); // add extra path and go to parent to canonicalize path 
+    if (!name || typeof name !== 'string') {
+      return failWithError("A module in the project file contains is missing a name or contains an invalid value.")
+    }
+    // TODO: check validity of module name
+    if (!source || typeof source !== 'string') {
+      return failWithError("The module '" + name + "' contains an invalid or missing value for its source directory.");
+    }
+    if (srcByModule[name]) {
+      return failWithError("The project file contains multiple definitions for the module '" + name + "'.");
+    }
+    let moduleDir = fs.getParent(fs.join(projectDir, source, 'dummypath')); // add extra path and go to parent to canonicalize path
     let files = {};
     let fileList = await fs.listRecursive(moduleDir, { useStructs: true });
     for (let file of fileList.filter(f => f.relative.toLowerCase().endsWith('.px'))) {
       let { relative, absolute } = file;
       let { ok, text } = await fs.fileReadText(absolute);
-      if (!ok) throw new Error('TODO: this should generally not happen (except in race conditions)');
+      if (!ok) {
+        return failWithError("There was an error while reading the source files. They were likely updated while compilation was occuring.");
+      }
       files[relative] = text;
     }
     srcByModule[name] = files;
   }
 
   if (!srcByModule[mainModule]) {
-    throw new Error('TODO: error for when the main module does not exist');
+    return failWithError("The main module '" + mainModule + "' does not exist in the list of modules provided in the project file.");
   }
 
   let compilationResult = await PlexiScript.compile(mainModule, srcByModule);
@@ -67,8 +79,9 @@ const APP_MAIN = async (os, procInfo, args) => {
     let dstParent = fs.getParent(dstPath);
     await fs.mkdirs(dstParent);
     await fs.fileWriteText(dstPath, compilationResult.byteCodeB64);
-    console.log("Compilation successful (TODO: remove this line)");
+  } else if (compilationResult.errorMessage) {
+    return failWithError('' + compilationResult.errorMessage);
   } else {
-    throw new Error("TODO: error for when there's no byte code");
+    throw new Error(); // should not happen.
   }
 };
